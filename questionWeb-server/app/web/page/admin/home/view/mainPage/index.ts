7e727f195ea32488a289a35f8caffb4bf27ab119
Hogzhu <https://Hogzhu@github.com>
2020-05-08 21:36:43 +0800

    hzf

--- questionWeb-server/app/web/page/admin/home/view/mainPage/index.ts
+++ questionWeb-server/app/web/page/admin/home/view/mainPage/index.ts
@@ -1,10 +1,18 @@
 import { Vue, Component } from 'vue-property-decorator';
 import QuestionList from '../../component/questionList/questionList.vue'
 import PersonalRank from '../../component/personalRank/personalRank.vue'
+import { checkLoginStatus } from "../../component/common/common";
 @Component({
   components: {
     QuestionList,
     PersonalRank
   }
 })
-export default class MainPage extends Vue {}
+export default class MainPage extends Vue {
+  private created() {
+    this.checkLogin()
+  }
+  private checkLogin() {
+    (Common as any).checkLoginStatus()
+  }
+}
